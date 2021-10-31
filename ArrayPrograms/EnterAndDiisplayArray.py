fe9ba3eb0d1ed19ca63c7b0959753facd97eb4f6
Organisationn <mano.sahoo91@gmail.com>
2021-10-31 20:52:02 +0530

    Enter elements to array at run time and display

---
+++ ArrayPrograms/EnterAndDiisplayArray.py
@@ -0,0 +1,20 @@
+from random import randrange
+
+sizeOfArray=int(input('Please enter the size of the array: '))
+
+numbersArray=[]
+count=0
+while(count<sizeOfArray):
+    try:
+        number=int(input('Enter a number: '))
+        numbersArray.append(number)
+        count += 1
+    except ValueError:
+        print('This is not a number bro..')
+        pass
+
+print(numbersArray)
+
+
+
+
